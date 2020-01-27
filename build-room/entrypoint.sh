9aee238f6d57bcd5459fdfdbbab8d5158965517a
Abdp <aai_hassan@hotmail.com>
2020-01-27 00:17:30 +0000

    move to build-room

---
+++ build-room/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+timestamp=$(date +"%Y%m%d")
+echo $timestamp
+
+cd /build-room/
+cmake -DVENDOR=espressif -DCMAKE_TOOLCHAIN_FILE=amazon-freertos/tools/cmake/toolchains/xtensa-esp32.cmake -DBOARD=esp32_wrover_kit -DCOMPILER=xtensa-esp32 -GNinja -S  . -B /build-output
+cd /build-output/ 
+ninja
